681ebe52bc8b9a288ce266e0b54656a986d84f91
Vinícius F.G Melo <viniciusferrazgmelo@outlook.com>
2022-11-18 15:03:04 -0300

    Possible moves of a piece

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -1,6 +1,7 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
+
     //Criando uma posição protected, pois não é a posição do xadrez
     //É uma posição simples de matriz
     protected Position position;
@@ -17,4 +18,23 @@ public class Piece {
         return board;
     }
 
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibelMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isthereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat.length; j++){
+                if(mat[i][j]){
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+            
 }
