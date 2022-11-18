5bbc42c32455f067aac352b54e579b3fb5f24a58
Vinícius F.G Melo <viniciusferrazgmelo@outlook.com>
2022-11-18 10:53:54 -0300

    chessException and ChessPosition

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -28,12 +27,15 @@ public class ChessMatch {
         return mat;
     }
     
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
     //Metodo responsável por iniciar a partida de xadrez
     //colocando as peças no tabuleiro
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
     }
-    
 }
+    
+    
