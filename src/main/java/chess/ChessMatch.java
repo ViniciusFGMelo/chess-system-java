2dbd4391b81d2301a1a91c6be4ae28d5331328e8
Vinícius F.G Melo <viniciusferrazgmelo@outlook.com>
2022-11-18 11:32:47 -0300

    Error on little improvement in board printing. Modifying initialSetup on ChessMatch. For somo reason, ocurring error from thereIsAPiece

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -5,37 +5,51 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-    
+
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
         initialSetup();
     }
-    
+
     //Metodo para retornar uma matriz de peças de xadrez correspondente a partida
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         //Percorrendo a matriz de peças do tabuleiro board
         //Para cada peça do tabuleiro, fazendo downcasting para chesspiece
-        for(int i=0; i<board.getRows(); i++){
-            for(int j=0; j<board.getColumns(); j++){
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 //Downcasting para ser interpretado como uma peça de xadrez
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
-    
-    private void placeNewPiece(char column, int row, ChessPiece piece){
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
+
     //Metodo responsável por iniciar a partida de xadrez
     //colocando as peças no tabuleiro
-    private void initialSetup(){
-        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+//    private void initialSetup(){
+//        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+//        placeNewPiece('e', 8, new King(board, Color.BLACK));
+//    }
+    private void initialSetup() {
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 }
-    
-    
